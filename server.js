f0b21f223d933f45f9162cf3ddb900b1480b5426
Josehpequeno <hicarojbs21@gmail.com>
2021-03-13 16:35:19 -0300

    correçao de datas

--- server.js
+++ server.js
@@ -11,42 +11,67 @@ var client = new Cassandra.Client({ contactPoints: contactPoints, localDataCente
 //Ensure all queries are executed before exit
 function execute(query, params) {
     return new Promise((resolve, reject) => {
-        //console.log("Paramentro: ", params.length);
-        if (params.length != 0) {
-            //console.log("Paramentro: ", params);
-            client.execute(query, params, (err, result) => {
-                if (err) {
-                    console.log(params);
-                    return reject(err);
-                }
-                return resolve(result);
-            });
+        /*console.log("{");
+        console.log("Paramentro: ", params);
+        console.log("Paramentro: ", params.length);
+        console.log("Query: ", query);
+        console.log("}");
+        */
+        client.execute(query, params,/* { prepare: true },*/(err, result) => {
+            if (err) {
+                console.log(params);
+                return reject(err);
+            }
+            return resolve(result);
+        });
+    });
+}
+
+function Registros() {
+    query = `SELECT * FROM registro.casamento;`;
+    q1 = execute(query, []).then(result => {
+        console.log();
+        console.log("########################################");
+        for (const row of result.rows) {
+            console.log(
+                "Linha: %s | %s | %s | %s ",
+                row.id,
+                row.name1,
+                row.name2,
+                row.data
+            );
         }
-        else {
-            //console.log("Paramentro vazio");
-            client.execute(query, (err, result) => {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(result);
-            });
+        console.log("########################################");
+        //client.shutdown();
+    }).catch(err => {
+        if (err != undefined) {
+            console.log(err);
         }
+        client.shutdown();
     });
 }
 
 //Execute the queries 
 var query = `CREATE KEYSPACE IF NOT EXISTS registro WITH REPLICATION = {'class' : 'SimpleStrategy','replication_factor' : 1};`
 var q1 = execute(query, []).then(result => {
-    console.log('Done');
+    console.log('Done Keyspace');
 }).catch(err => {
     console.log(err);
 });
-query = `CREATE TABLE IF NOT EXISTS registro.casamento (id VARCHAR, name1 TEXT, name2 TEXT, data date, PRIMARY KEY (id));`
+query = `CREATE TABLE IF NOT EXISTS registro.casamento (id VARCHAR, name1 TEXT, name2 TEXT, data DATE, PRIMARY KEY (id));`
 q1 = execute(query, []).then(result => {
-    console.log('Done');
+    console.log('Done Table');
 }).catch(err => {
     console.log(err);
 });
+query = `DELETE FROM registro.casamento WHERE id IN ('a1', 'b2','c3','d4');`
+q1 = execute(query, []).then(result => {
+    console.log('Done Delete');
+    Registros();
+}).catch(err => {
+    console.log(err);
+});
+
 let data = new Date("Jan 29, 2023");
 const meses = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
 let dia = data.getDate();
@@ -54,25 +79,31 @@ if (dia < 10) {
     dia = String("0" + dia);
 }
 data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
-console.log(data);
-data.split("-");
-query = `INSERT INTO registro.casamento (id,name1, name2, data) VALUES ('a1','Joseh','Marcus',???);`
-q1 = execute(query, [data[0],data[1],data[2]]).then(result => {
-    console.log('Done');
+//console.log(data);
+data = data;
+query = `INSERT INTO registro.casamento (id,name1, name2, data) VALUES ('a1','Joseh','Marcus', '${data}');`
+q1 = execute(query, []).then(result => {
+    console.log('Done Insert-0');
 }).catch(err => {
     console.log(err);
 });
-data = new Date("Apr 2, 2016");
+data = new Date("Jan 24, 2017");
 dia = data.getDate();
 if (dia < 10) {
     dia = String("0" + dia);
 }
 data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
-console.log(data);
-data.split("-");
-query = `INSERT INTO registro.casamento (id, name1, name2, data) VALUES ('b2','John','Esqueci',???);`
-q1 = execute(query, [data[0],data[1],data[2]]).then(result => {
-    console.log('Done');
+//console.log(data);
+query = `INSERT INTO registro.casamento (id, name1, name2, data) VALUES ('b2','Jhon','Camila','${data}');`
+q1 = execute(query, []).then(result => {
+    console.log('Done Insert-1');
+}).catch(err => {
+    console.log(err);
+});
+query = `INSERT INTO registro.casamento (id, name1, name2) VALUES ('c3','Hiago','Taina');`
+q1 = execute(query, []).then(result => {
+    console.log('Done Insert-2');
+    Registros();
 }).catch(err => {
     console.log(err);
 });
@@ -83,25 +114,26 @@ q1 = execute(query, ['oranges']).then(result => {
 }).catch(err => {
     console.log(err)
 });*/
-query = `SELECT * FROM registro.casamento;`;
+data = new Date("Sept 18, 2022");
+dia = data.getDate();
+if (dia < 10) {
+    dia = String("0" + dia);
+}
+data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
+
+query = `INSERT INTO registro.casamento (id, name1, name2,data) VALUES ('c3','Hiago','Taina','${data}');`
 q1 = execute(query, []).then(result => {
-    console.log("#######################################################");
-    for (const row of result.rows) {
-        console.log(
-            "Linha de Casamentos: %s | %s | %s | %s ",
-            row.id,
-            row.name1,
-            row.name2,
-            row.data
-        );
-    }
-    console.log("#######################################################");
-    //client.shutdown();
+    console.log('Done Insert-3');
+    Registros();
 }).catch(err => {
-    if (err != undefined) {
-        console.log(err);
-    }
-    //client.shutdown();
+    console.log(err);
+});
+query = `UPDATE registro.casamento SET data = '${'2022-03-25'}' WHERE id = 'c3';`
+q1 = execute(query, []).then(result => {
+    console.log('Done Update');
+    Registros();
+}).catch(err => {
+    console.log(err);
 });
 /*require('dotenv').config();
 const cassandra = require('cassandra-driver');
