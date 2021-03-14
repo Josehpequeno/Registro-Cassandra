4998d3ea6e1f8d61d313bee82d8e10b8d7fbcaa6
Josehpequeno <hicarojbs21@gmail.com>
2021-03-14 02:17:45 -0300

    delete ok, problema com sequencia de promises

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 //”cassandra-driver” is in the node_modules folder. Redirect if necessary.
 var Cassandra = require('cassandra-driver');
+const { resolve } = require('path');
 //Replace Username and Password with your cluster settings
 //var authProvider = new cassandra.auth.PlainTextAuthProvider('Username', 'Password');
 //Replace PublicIP with the IP addresses of your clusters
@@ -27,129 +28,115 @@ function execute(query, params) {
     });
 }
 
-function Registros() {
-    query = `SELECT * FROM registro.casamento;`;
-    q1 = execute(query, []).then(result => {
-        console.log();
-        console.log("########################################");
-        for (const row of result.rows) {
-            console.log(
-                "Linha: %s | %s | %s | %s ",
-                row.id,
-                row.name1,
-                row.name2,
-                row.data
-            );
-        }
-        console.log("########################################");
-        //client.shutdown();
-    }).catch(err => {
-        if (err != undefined) {
-            console.log(err);
-        }
-        client.shutdown();
+function Registros(q) {
+    return new Promise((resolve, reject) => {
+        query = `SELECT * FROM registro.casamento;`;
+        q1 = execute(query, []).then(result => {
+            console.log();
+            console.log("########################################");
+            console.log("Query: ", q)
+            for (const row of result.rows) {
+                console.log(
+                    "Linha: %s | %s | %s | %s ",
+                    row.id,
+                    row.name1,
+                    row.name2,
+                    row.data
+                );
+            }
+            console.log("########################################");
+            resolve()
+            //client.shutdown();
+        }).catch(err => {
+            if (err != undefined) {
+                console.log(err);
+                reject(err);
+            }
+            client.shutdown();
+        });
     });
 }
 
-//Execute the queries 
-var query = `CREATE KEYSPACE IF NOT EXISTS registro WITH REPLICATION = {'class' : 'SimpleStrategy','replication_factor' : 1};`
-var q1 = execute(query, []).then(result => {
-    console.log('Done Keyspace');
-}).catch(err => {
-    console.log(err);
-});
-query = `CREATE TABLE IF NOT EXISTS registro.casamento (id VARCHAR, name1 TEXT, name2 TEXT, data DATE, PRIMARY KEY (id));`
-q1 = execute(query, []).then(result => {
-    console.log('Done Table');
-}).catch(err => {
-    console.log(err);
-});
-query = `DELETE FROM registro.casamento WHERE id IN ('a1', 'b2','c3','d4');`
-q1 = execute(query, []).then(result => {
-    console.log('Done Delete');
-    Registros();
-}).catch(err => {
-    console.log(err);
-});
+//Execute the queries
+let promisesInicial = [];
+var query1 = `CREATE KEYSPACE IF NOT EXISTS registro WITH REPLICATION = {'class' : 'SimpleStrategy','replication_factor' : 1};`
+var q1 = execute(query1, []);
+promisesInicial.push(q1);
+var query2 = `CREATE TABLE IF NOT EXISTS registro.casamento (id VARCHAR, name1 TEXT, name2 TEXT, data DATE, PRIMARY KEY (id));`
+var q2 = execute(query2, []);
+promisesInicial.push(q2);
+var query3 = `DELETE FROM registro.casamento WHERE id = 'a1';`
+var q3 = execute(query3, []);
+var query4 = `DELETE FROM registro.casamento WHERE id = 'b2';`
+var q4 = execute(query4, []);
+var query5 = `DELETE FROM registro.casamento WHERE id = 'c3';`
+var q5 = execute(query5, []);
 
-let data = new Date("Jan 29, 2023");
-const meses = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-let dia = data.getDate();
-if (dia < 10) {
-    dia = String("0" + dia);
-}
-data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
-//console.log(data);
-data = data;
-query = `INSERT INTO registro.casamento (id,name1, name2, data) VALUES ('a1','Joseh','Marcus', '${data}');`
-q1 = execute(query, []).then(result => {
-    console.log('Done Insert-0');
-}).catch(err => {
-    console.log(err);
-});
-data = new Date("Jan 24, 2017");
-dia = data.getDate();
-if (dia < 10) {
-    dia = String("0" + dia);
-}
-data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
-//console.log(data);
-query = `INSERT INTO registro.casamento (id, name1, name2, data) VALUES ('b2','Jhon','Camila','${data}');`
-q1 = execute(query, []).then(result => {
-    console.log('Done Insert-1');
-}).catch(err => {
-    console.log(err);
-});
-query = `INSERT INTO registro.casamento (id, name1, name2) VALUES ('c3','Hiago','Taina');`
-q1 = execute(query, []).then(result => {
-    console.log('Done Insert-2');
-    Registros();
-}).catch(err => {
-    console.log(err);
-});
-/*
-query = `SELECT name, price_p_item FROM registro.casamento WHERE name=? ALLOW FILTERING`;
-q1 = execute(query, ['oranges']).then(result => {
-    console.log('The cost per orange is', Number(result.rows[0].price_p_item));
+promisesInicial.push(q3);
+promisesInicial.push(q4);
+promisesInicial.push(q5);
+promisesInicial.push(Registros([query1, query2, query3, query4, query5]));
+const promisesAll = Promise.all(promisesInicial);
+promisesAll.then((result) => {
+    console.log("Feito querys iniciais");
+    let promises = [];
+
+    let data = new Date("Jan 29, 2023");
+    const meses = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+    let dia = data.getDate();
+    if (dia < 10) {
+        dia = String("0" + dia);
+    }
+    data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
+    //console.log(data);
+    data = data;
+    query1 = `INSERT INTO registro.casamento (id,name1, name2, data) VALUES ('a1','Joseh','Marcus', '${data}');`
+    q1 = execute(query1, []);
+    data = new Date("Jan 24, 2017");
+    dia = data.getDate();
+    if (dia < 10) {
+        dia = String("0" + dia);
+    }
+    data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
+    //console.log(data);
+    query2 = `INSERT INTO registro.casamento (id, name1, name2, data) VALUES ('b2','Jhon','Camila','${data}');`
+    q2 = execute(query2, []);
+    query3 = `INSERT INTO registro.casamento (id, name1, name2) VALUES ('c3','Hiago','Taina');`
+    q3 = execute(query3, []);
+    data = new Date("Sept 18, 2022");
+    dia = data.getDate();
+    if (dia < 10) {
+        dia = String("0" + dia);
+    }
+    data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
+    
+    query4 = `INSERT INTO registro.casamento (id, name1, name2,data) VALUES ('c3','Hiago','Taina','${data}');`
+    q4 = execute(query4, []);
+    query5 = `UPDATE registro.casamento SET data = '${'2022-03-25'}' WHERE id = 'c3';`
+    q5 = execute(query5, []);
+    promises.push(q1);
+    promises.push(Registros(query1));
+    promises.push(q2);
+    promises.push(Registros(query2));
+    promises.push(q3);
+    promises.push(Registros(query3));
+    promises.push(q4);
+    promises.push(Registros(query4));
+    promises.push(q5);
+    promises.push(Registros(query5));
+    const promisesAll2 = Promise.all(promises);
+    promisesAll2.then(result => {
+        console.log("Querys secundárias feitas");
+    }).catch(err => {
+        console.log(err);
+    })
 }).catch(err => {
     console.log(err)
-});*/
-data = new Date("Sept 18, 2022");
-dia = data.getDate();
-if (dia < 10) {
-    dia = String("0" + dia);
-}
-data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
-
-query = `INSERT INTO registro.casamento (id, name1, name2,data) VALUES ('c3','Hiago','Taina','${data}');`
-q1 = execute(query, []).then(result => {
-    console.log('Done Insert-3');
-    Registros();
-}).catch(err => {
-    console.log(err);
 });
-query = `UPDATE registro.casamento SET data = '${'2022-03-25'}' WHERE id = 'c3';`
-q1 = execute(query, []).then(result => {
-    console.log('Done Update');
-    Registros();
-}).catch(err => {
-    console.log(err);
-});
-/*require('dotenv').config();
-const cassandra = require('cassandra-driver');
-const authProvider = new cassandra.auth.PlainTextAuthProvider(
-    process.env.CASSANDRA_USER,
-    process.env.CASSANDRA_PASS,
-);
-const client = new cassandra.Client({
-    contactPoints: ['ip-address, ip-address, ip-address'],
-    localDataCenter: 'datacentername',
-    authProvider,
-    keyspace: 'keyspacename',
-});
-//client.execute(query, parameters, options)
-const findAllByListing = async (parameters) => {
-    const query = 'SELECT * FROM reviews_by_listing WHERE listing_id   = ?;';
-    const result = await client.execute(query, parameters, { prepare: true });
-    return result;
-};*/
+/*
+    query = `SELECT name, price_p_item FROM registro.casamento WHERE name=? ALLOW FILTERING`;
+    q1 = execute(query, ['oranges']).then(result => {
+        console.log('The cost per orange is', Number(result.rows[0].price_p_item));
+    }).catch(err => {
+        console.log(err)
+    });*/
