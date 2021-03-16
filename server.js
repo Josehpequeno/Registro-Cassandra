8d4023abce72db653d55b2ad2be0ebffb362f305
Josehpequeno <hicarojbs21@gmail.com>
2021-03-15 23:52:41 -0300

    erro de tabela nao configurada corrigido

--- server.js
+++ server.js
@@ -89,64 +89,67 @@ var q5 = execute(query5, []);
 promisesInicial.push(q3);
 promisesInicial.push(q4);
 promisesInicial.push(q5);
-promisesInicial.push(Registros([query1, query2, query3, query4, query5]));
 const promisesAll = Promise.all(promisesInicial);
 promisesAll.then((result) => {
-    console.log("Feito querys iniciais");
-    let promises = [];
+    Registros([query1, query2, query3, query4, query5]).then(result => {
 
-    let data = new Date("Jan 29, 2023");
-    const meses = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-    let dia = data.getDate();
-    if (dia < 10) {
-        dia = String("0" + dia);
-    }
-    data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
-    //console.log(data);
-    data = data;
-    query1 = `INSERT INTO registro.casamento (id,name1, name2, data) VALUES ('a1','Joseh','Marcus','${data}');`
-    q1 = execute(query1, []);
-    data = new Date("Jan 24, 2017");
-    dia = data.getDate();
-    if (dia < 10) {
-        dia = String("0" + dia);
-    }
-    data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
-    //console.log(data);
-    query2 = `INSERT INTO registro.casamento (id, name1, name2, data) VALUES ('b2','Jhon','Camila','${data}');`
-    q2 = execute(query2, []);
-    query3 = `INSERT INTO registro.casamento (id, name1, name2) VALUES ('c3','Hiago','Taina');`;
-    q3 = execute(query3, []);
-    query4 = `INSERT INTO registro.casamento (id, name1) VALUES ('d4','Rodman');`;
-    q4 = execute(query4, []);
-    promises.push(q1);
-    promises.push(q2);
-    promises.push(q3);
-    promises.push(q4);
-    const promisesAll2 = Promise.all(promises);
-    promisesAll2.then((result) => {
-        Registros([query1, query2, query3, query4]).then(result => {
-            data = new Date("Sept 18, 2022");
-            dia = data.getDate();
-            if (dia < 10) {
-                dia = String("0" + dia);
-            }
-            data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
 
-            query4 = `INSERT INTO registro.casamento (id, data) VALUES ('c3','${data}');`
-            q4 = execute(query4, []).then(result => {
-                Registros([query4]).then(result => {
-                    query5 = `UPDATE registro.casamento SET data = '${'2022-03-25'}' WHERE id = 'c3';`
-                    q5 = execute(query5, []).then(result => {
-                        Registros([query5]).then(result => {
-                            console.log("");
-                            console.log("Querys secundárias feitas");
-                        })
-                    }).catch(err => { console.log(err) });
-                });
+        console.log("Feito querys iniciais");
+        let promises = [];
+
+        let data = new Date("Jan 29, 2023");
+        const meses = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+        let dia = data.getDate();
+        if (dia < 10) {
+            dia = String("0" + dia);
+        }
+        data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
+        //console.log(data);
+        data = data;
+        query1 = `INSERT INTO registro.casamento (id,name1, name2, data) VALUES ('a1','Joseh','Marcus','${data}');`
+        q1 = execute(query1, []);
+        data = new Date("Jan 24, 2017");
+        dia = data.getDate();
+        if (dia < 10) {
+            dia = String("0" + dia);
+        }
+        data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
+        //console.log(data);
+        query2 = `INSERT INTO registro.casamento (id, name1, name2, data) VALUES ('b2','Jhon','Camila','${data}');`
+        q2 = execute(query2, []);
+        query3 = `INSERT INTO registro.casamento (id, name1, name2) VALUES ('c3','Hiago','Taina');`;
+        q3 = execute(query3, []);
+        query4 = `INSERT INTO registro.casamento (id, name1) VALUES ('d4','Rodman');`;
+        q4 = execute(query4, []);
+        promises.push(q1);
+        promises.push(q2);
+        promises.push(q3);
+        promises.push(q4);
+        const promisesAll2 = Promise.all(promises);
+        promisesAll2.then((result) => {
+            Registros([query1, query2, query3, query4]).then(result => {
+                data = new Date("Sept 18, 2022");
+                dia = data.getDate();
+                if (dia < 10) {
+                    dia = String("0" + dia);
+                }
+                data = String((data.getFullYear() + "-" + meses[(data.getMonth())] + "-" + dia));
+
+                query4 = `INSERT INTO registro.casamento (id, data) VALUES ('c3','${data}');`
+                q4 = execute(query4, []).then(result => {
+                    Registros([query4]).then(result => {
+                        query5 = `UPDATE registro.casamento SET data = '${'2022-03-25'}' WHERE id = 'c3';`
+                        q5 = execute(query5, []).then(result => {
+                            Registros([query5]).then(result => {
+                                console.log("");
+                                console.log("Querys secundárias feitas");
+                            })
+                        }).catch(err => { console.log(err) });
+                    });
+                }).catch(err => { console.log(err) });
             }).catch(err => { console.log(err) });
-        }).catch(err => { console.log(err) });
-    }).catch(err => { console.log });
+        }).catch(err => { console.log });
+    }).catch(err => { console.log(err); });
 }).catch(err => {
     console.log(err)
 });
